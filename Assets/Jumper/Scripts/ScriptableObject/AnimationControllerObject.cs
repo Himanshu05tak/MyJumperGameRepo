2f6d36309c6008cf6d44d43178a0eb4071bd6b94
Himanshu <himanshu05.tak@gmail.com>
2020-08-28 16:18:36 +0530

    Organize and Updated
    Removed Unnecessary Assests and The project has been updated

--- Assets/Jumper/Scripts/ScriptableObject/AnimationControllerObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-namespace Jumper.Rework.Scriptable
-{
-    [CreateAssetMenu(fileName = "JumperAssets", menuName = "Controller/Animation")]
-    public class AnimationControllerObject : ScriptableObject
-    {
-        public Animator Anim;
-    }
-}
